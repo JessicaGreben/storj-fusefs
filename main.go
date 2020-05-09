0b30dfdc5b7c7bdb97f48f06e7b8a49ca556852e
Jessica Grebenschikov <Jessica.greben1+github@gmail.com>
2020-05-09 12:39:39 -0700

    handle dirs

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/user"
 	"path/filepath"
-	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -17,6 +16,11 @@ import (
 	"bazil.org/fuse/fs"
 	_ "bazil.org/fuse/fs/fstestutil"
 	"storj.io/uplink"
+)
+
+var (
+	uid = uint32(os.Geteuid())
+	gid = uint32(os.Getegid())
 )
 
 func main() {
@@ -59,7 +63,7 @@ var _ fs.FS = (*FS)(nil)
 
 func NewFS(project *uplink.Project, bucketname string) *FS {
 	return &FS{
-		root: NewDir(project, bucketname),
+		root: NewDir(project, bucketname, ""),
 	}
 }
 
@@ -68,8 +72,9 @@ func (fs FS) Root() (fs.Node, error) {
 }
 
 type Dir struct {
-	bucketname string
 	project    *uplink.Project
+	bucketname string
+	prefix     string
 }
 
 // fs.Node is the interface required of a file or directory.
@@ -77,46 +82,46 @@ var _ fs.Node = (*Dir)(nil)
 
 var _ fs.HandleReadDirAller = (*Dir)(nil)
 
-func NewDir(project *uplink.Project, bucketname string) *Dir {
+func NewDir(project *uplink.Project, bucketname, prefix string) *Dir {
 	return &Dir{
+		prefix:     prefix,
 		bucketname: bucketname,
 		project:    project,
 	}
 }
 
 func (d *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
-	start := time.Now()
 	a.Mode = os.ModeDir | 0o555
-	a.Valid = 5 * time.Minute
-	a.Uid = getUserID()
-	a.Gid = getGroupID()
-	log.Println(time.Since(start).Milliseconds(), " (ms), dir Attr")
+	a.Uid = uid
+	a.Gid = gid
 	return nil
 }
 
-// A LookupRequest asks to look up the given name in the directory named by r.Node.
-func (d *Dir) Lookup(ctx context.Context, objName string) (fs.Node, error) {
+func (d *Dir) Lookup(ctx context.Context, objKey string) (fs.Node, error) {
 	start := time.Now()
-	if f, ok := d.containsObject(ctx, objName); ok {
-		return f, nil
-	}
-	log.Println(time.Since(start).Milliseconds(), " (ms), dir lookup for object", objName)
-	return nil, syscall.ENOENT
-}
 
-func (d *Dir) containsObject(ctx context.Context, objName string) (*File, bool) {
-	iter := d.project.ListObjects(ctx, d.bucketname, nil)
-	for iter.Next() {
-		if iter.Item().Key == objName {
-			f := newFile(iter.Item(), d.project, d.bucketname)
-			return f, true
+	object, err := d.project.StatObject(ctx, d.bucketname, objKey)
+	if err != nil {
+		if strings.Contains(err.Error(), "object not found") {
+			fmt.Printf("err: %#v\n", err)
+			return nil, syscall.ENOENT
 		}
+		return nil, err
 	}
-	if err := iter.Err(); err != nil {
-		log.Fatal("listObj ", err)
-		return nil, false
+
+	if object.IsPrefix {
+		d := NewDir(d.project, d.bucketname, object.Key)
+		log.Println(time.Since(start).Milliseconds(),
+			" ms, dir lookup for object", objKey,
+		)
+		return d, nil
 	}
-	return nil, false
+
+	f := newFile(object, d.project, d.bucketname)
+	log.Println(time.Since(start).Milliseconds(),
+		" ms, dir lookup for object", objKey,
+	)
+	return f, nil
 }
 
 func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
@@ -130,6 +135,9 @@ func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 			Name: iter.Item().Key,
 			Type: fuse.DT_File,
 		}
+		if iter.Item().IsPrefix {
+			entry.Type = fuse.DT_Dir
+		}
 		dirDirs = append(dirDirs, entry)
 	}
 	if err := iter.Err(); err != nil {
@@ -137,7 +145,7 @@ func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 		return dirDirs, err
 	}
 
-	log.Println(time.Since(start).Milliseconds(), "(ms), dir ReadDirAll")
+	log.Println(time.Since(start).Milliseconds(), "ms, dir ReadDirAll")
 	return dirDirs, nil
 }
 
@@ -162,17 +170,16 @@ func newFile(obj *uplink.Object, project *uplink.Project, bucketname string) *Fi
 
 func (f *File) Attr(ctx context.Context, a *fuse.Attr) error {
 	start := time.Now()
-	a.Valid = 5 * time.Minute
-	a.Mode = 0o444 // read only
-	a.Uid = getUserID()
-	a.Gid = getGroupID()
+	a.Mode = 0o444
+	a.Uid = uid
+	a.Gid = gid
 
 	s, err := f.project.StatObject(ctx, f.bucketname, f.obj.Key)
 	if err != nil {
 		log.Fatal("object stat: ", err)
 	}
 	a.Size = uint64(s.System.ContentLength)
-	log.Println(time.Since(start).Milliseconds(), "(ms), file Attr for", f.obj.Key)
+	log.Println(time.Since(start).Milliseconds(), "ms, file Attr for", f.obj.Key)
 	return nil
 }
 
@@ -188,24 +195,6 @@ func (f *File) ReadAll(ctx context.Context) ([]byte, error) {
 	if err != nil {
 		log.Fatal("readAll: ", err)
 	}
-	log.Println(time.Since(start).Milliseconds(), "(ms), file ReadAll for", f.obj.Key)
+	log.Println(time.Since(start).Milliseconds(), "ms, file ReadAll for", f.obj.Key)
 	return b, nil
-}
-
-func getUserID() uint32 {
-	// set user and group to the current user/group
-	// otherwise defaults to root
-	user, err := user.Current()
-	if err != nil {
-		log.Fatal("current user: ", err)
-	}
-	uid, err := strconv.Atoi(user.Uid)
-	if err != nil {
-		log.Fatal("atoi uid: ", err)
-	}
-	return uint32(uid)
-}
-func getGroupID() uint32 {
-	// todo: dont hardcore
-	return uint32(1001)
 }
